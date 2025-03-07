65954bae79f619c3f32169e03155c2aa7a604253
EzazAA <ezazalamahmed@gmail.com>
2025-03-07 22:32:47 +0530

    Done broooooooo

--- tut4.py
+++ tut4.py
@@ -11,6 +11,10 @@ print(len(value))
 second_name = value[6:12] #from ind_start(6) to ind_end(12)-1
 print(second_name)
 
+skip_slicing = value[0::2]
+# take the whole ind_start to ind_end string and jump last_value characters in the string 
+print(skip_slicing)
+
 # 2. getting a certain character 
 char = value[2]
 print(char)
